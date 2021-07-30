53fed0db01658210666275e96b7b291c1b2aeb27
harjeet-dhingwani <88183311+harjeet-dhingwani@users.noreply.github.com>
2021-07-30 16:50:29 +0530

    push all root files commit

---
+++ export.php
@@ -0,0 +1,25 @@
+<?php
+$conn = mysqli_connect("localhost", "root", "", "lfg");
+
+$filename = "export_games.csv";
+$fp = fopen('php://output', 'w');
+/*
+$query = "SELECT COLUMN_NAME FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_SCHEMA='lfg' AND TABLE_NAME='LFG_Game'";
+$result = mysqli_query($conn,$query);
+while ($row = mysqli_fetch_row($result)) {
+	$header[] = $row[0];
+}	*/
+
+$header = array('id'  , 'title', 'category', 'description', 'image_url'); 
+
+header('Content-type: application/csv');
+header('Content-Disposition: attachment; filename='.$filename);
+fputcsv($fp, $header);
+
+$query = "SELECT game.id, game.title,CONCAT(ge.title, '>', type.title), game.description,  game.imageURL FROM `LFG_Game` as game left join LFG_GameType as type on type.id = game.type_id left JOIN LFG_GameGenre as ge on ge.id = game.genre_id ORDER BY `game`.`genre_id` ASC ";
+$result = mysqli_query($conn, $query);
+while($row = mysqli_fetch_row($result)) {
+	fputcsv($fp, $row);
+}
+exit;
+?>
